5aec5b160971b62418c7d9a742dd35c3903fb034
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2026-03-11 23:22:17 +0700

    style: adjust heading style

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -16,77 +16,35 @@ export default function Home({ params }: HomeProps) {
       <Heading id="test" level={1}>
         Ini heading afafa asfsf
       </Heading>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum velit natus ipsa nihil odio praesentium quos quod blanditiis sit beatae harum id architecto error quam, sed tenetur eveniet labore dolorem.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum velit natus ipsa nihil odio praesentium quos quod blanditiis sit beatae harum id architecto error quam, sed tenetur eveniet labore dolorem.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum velit natus ipsa nihil odio praesentium quos quod blanditiis sit beatae harum id architecto error quam, sed tenetur eveniet labore dolorem.
+      </p>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum velit natus ipsa nihil odio praesentium quos quod blanditiis sit beatae harum id architecto error quam, sed tenetur eveniet labore dolorem.
+      </p>
+      <Heading id="test2">
+        Ini heading 2
+      </Heading>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum velit natus ipsa nihil odio praesentium quos quod blanditiis sit beatae harum id architecto error quam, sed tenetur eveniet labore dolorem.
+      </p>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum velit natus ipsa nihil odio praesentium quos quod blanditiis sit beatae harum id architecto error quam, sed tenetur eveniet labore dolorem.
+      </p>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum velit natus ipsa nihil odio praesentium quos quod blanditiis sit beatae harum id architecto error quam, sed tenetur eveniet labore dolorem.
+      </p>
+      <Heading id="test3" level={3}>
+        Ini heading 3
+      </Heading>
+      <Heading id="test4" level={4}>
+        Ini heading 4
+      </Heading>
+      <br />
+      <h1>afafas</h1>
+      <h2>safasfsa</h2>
     </div>
-    // <div className="flex flex-col">
-    //   <Heading id="test">
-    //     Ini heading 2
-    //   </Heading>
-    //   <Heading id="test" level={3}>
-    //     Ini heading 3
-    //   </Heading>
-    //   <Heading id="test" level={4}>
-    //     Ini heading 4
-    //   </Heading>
-    // </div>
-    // <div className="flex min-h-screen items-center justify-center  font-sans">
-    //   {/* <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-    //     <h1 className="text-6xl font-bold text-center mb-8">Title</h1>
-    //     <Image
-    //       className="dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js logo"
-    //       width={100}
-    //       height={20}
-    //       priority
-    //     />
-    //     <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-    //       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-    //         To get started, edit the page.tsx file.
-    //       </h1>
-    //       <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-    //         Looking for a starting point or more instructions? Head over to{" "}
-    //         <a
-    //           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //           className="font-medium text-zinc-950 dark:text-zinc-50"
-    //         >
-    //           Templates
-    //         </a>{" "}
-    //         or the{" "}
-    //         <a
-    //           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //           className="font-medium text-zinc-950 dark:text-zinc-50"
-    //         >
-    //           Learning
-    //         </a>{" "}
-    //         center.
-    //       </p>
-    //     </div>
-    //     <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-    //       <a
-    //         className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors duration-300 ease hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         <Image
-    //           className="dark:invert"
-    //           src="/vercel.svg"
-    //           alt="Vercel logomark"
-    //           width={16}
-    //           height={16}
-    //         />
-    //         Deploy Now
-    //       </a>
-    //       <a
-    //         className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors duration-300 ease hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Documentation
-    //       </a>
-    //     </div>
-    //   </main> */}
-    // </div>
   );
 }
--- components/Heading.tsx
+++ components/Heading.tsx
@@ -2,14 +2,15 @@
 
 import clsx from "clsx"
 import { toSnakeCase } from "@/utils/stringCase"
-import React, { JSX, useState } from "react"
+import React, { JSX } from "react"
 import MiracleTooltip from "./miracle/Tooltip"
 import { LuLink2 } from "react-icons/lu"
 import { useCopyToClipboard } from "@/hooks/useCopyToClipboard"
+import { Link } from "@/i18n/navigation"
 
 export type HeadingProps = {
   id?: string
-  level?: 1 | 2 | 3 | 4
+  level?: 1 | 2 | 3 | 4 | 5
   children: React.ReactNode
   className?: string
   copyLink?: boolean
@@ -36,23 +37,26 @@ export default function Heading({
   }
 
   const levelClass: Record<number, string> = {
-    1: "text-5xl",
-    2: "text-3xl",
-    3: "text-2xl",
-    4: "text-xl",
+    1: "text-5xl mt-0",
+    2: "text-4xl mt-10",
+    3: "text-3xl mt-8",
+    4: "text-2xl mt-6",
+    5: "text-xl mt-4"
   }
 
   return (
     <Tag 
       id={headingId} 
       className={clsx(
-        "relative font-medium leading-tight inline-block group/heading",
+        "relative font-medium leading-tight group/heading scroll-mt-24",
         levelClass[level],
         className
       )}>
-      {children}
+      <Link href={`#${headingId}`}>
+        {children}
+      </Link>
       {copyLink && (
-        <div className="absolute right-full top-1/2 -translate-y-1/2 pr-2 invisible opacity-0 translate-x-1 group-hover/heading:translate-x-0 group-hover/heading:visible group-hover/heading:opacity-100 transition duration-300 ease shrink-0 flex-1">
+        <div className="absolute right-full top-1/2 -translate-y-1/2 pr-1 invisible opacity-0 translate-x-1 group-hover/heading:translate-x-0 group-hover/heading:visible group-hover/heading:opacity-100 transition duration-300 ease shrink-0 flex-1">
           <MiracleTooltip
             position="top-start"
             trigger={
@@ -62,7 +66,7 @@ export default function Heading({
                 aria-label="Copy link to this section"
                 title="Copy link"
               >
-                <LuLink2 className="text-[0.3em]"/>
+                <LuLink2 className="text-[0.5em]"/>
               </button>
             }>
             <span className="flex text-nowrap text-xs">{message}</span>
