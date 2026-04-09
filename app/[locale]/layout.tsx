865fddea8de8175e713e997b048c67b6406f6a08
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2026-04-09 22:57:57 +0700

    remove ssg

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -28,17 +28,9 @@ export const viewport: Viewport = {
   colorScheme: "light dark",
 }
 
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }))
-}
-
 export default async function LocaleLayout({ children, params }: LayoutProps) {
   const { locale } = await params
-  if (!hasLocale(routing.locales, locale)) {
-    notFound()
-  }
-  setRequestLocale(locale)
-
+  
   let user = null
 
   try {
