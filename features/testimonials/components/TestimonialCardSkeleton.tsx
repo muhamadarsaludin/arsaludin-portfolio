fcbfc3497c9e0639ccf0e25e02c42b4989bfd5ea
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2026-04-15 13:22:06 +0700

    style: adjust testimonial card skeleton

--- features/testimonials/components/TestimonialCardSkeleton.tsx
+++ features/testimonials/components/TestimonialCardSkeleton.tsx
@@ -8,14 +8,17 @@ export default function TestimonialCardSkeleton() {
         "flex w-[80vw] max-w-[300px] shrink-0 snap-start flex-col overflow-hidden sm:w-auto sm:max-w-none"
       )}
     >
-      <MiracleSkeleton className="aspect-7/5 w-full rounded-2xl!" variant="med" />
-      <div className="flex flex-1 gap-2 p-4 items-center">
-        <MiracleSkeleton className="h-10 w-10" variant="med" />
+      <MiracleSkeleton className="w-full h-35 rounded-2xl!" variant="med" />
+      <div className="flex flex-1 gap-2 pt-5 items-center">
+        <MiracleSkeleton className="h-10 w-10 rounded-full!" variant="med" />
         <div className="flex flex-col flex-1">
           <MiracleSkeleton className="mb-2 h-4 w-full" variant="med" />
           <MiracleSkeleton className="h-3 w-3/4" />
         </div>
-        <MiracleSkeleton className="h-6 w-6 rounded-full! ml-2" variant="med" />
+        <div className="flex items-center gap-1 ml-2">
+          <MiracleSkeleton className="h-6 w-6 rounded-full!" variant="med" />
+          <MiracleSkeleton className="h-6 w-6 rounded-full!" variant="med" />
+        </div>
       </div>
     </div>
   )
