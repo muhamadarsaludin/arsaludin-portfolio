5b9f9ba0fa1a23d2d9aa89b036a6ec6d1be52c33
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2026-04-30 20:30:30 +0700

    feat: i18n  privacy policy badge latest update

--- features/privacy-policy/components/PrivacyPolicyPage.tsx
+++ features/privacy-policy/components/PrivacyPolicyPage.tsx
@@ -8,6 +8,7 @@ import TableOfContents from '@/components/TableOfContents';
 import Article from '@/components/Article';
 import MiracleBadge from '@/components/miracle/Badge';
 import { LuCalendar } from 'react-icons/lu';
+import { formatDate } from '@/utils/format-date';
 
 type PrivacyPolicyPageProps = {
   params: Promise<{ locale: string }>;
@@ -27,6 +28,8 @@ export default async function PrivacyPolicyPage({params}: PrivacyPolicyPageProps
     notFound();
   }
 
+  const LATEST_UPDATE_DATE = "30-04-2026"
+
   return (
     <Container className="flex gap-6 md:gap-8 items-start">
       <Article className="pb-13 lg:pb-23 flex-1">
@@ -39,7 +42,7 @@ export default async function PrivacyPolicyPage({params}: PrivacyPolicyPageProps
           className="mb-5 md:mb-6"
         />
         <Section className="w-full">
-          <MiracleBadge startIcon={<LuCalendar/>} className="mb-4">Last Updated: April 24, 2026</MiracleBadge>
+          <MiracleBadge startIcon={<LuCalendar/>} className="mb-4">{t("latestUpdate", {date: formatDate({date: LATEST_UPDATE_DATE, locale})})}</MiracleBadge>
           <Content />
         </Section>
       </Article>
