c9922dc51ecf6223acf18485a1d75ff5f3706325
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2026-05-09 15:01:10 +0700

    reveal privacy policy

--- features/privacy-policy/components/PrivacyPolicyPage.tsx
+++ features/privacy-policy/components/PrivacyPolicyPage.tsx
@@ -9,6 +9,7 @@ import Article from '@/components/Article';
 import MiracleBadge from '@/components/miracle/Badge';
 import { LuCalendar } from 'react-icons/lu';
 import { formatDate } from '@/utils/format-date';
+import { MiracleReveal } from '@/components/miracle/Reveal';
 
 type PrivacyPolicyPageProps = {
   params: Promise<{ locale: string }>;
@@ -33,18 +34,20 @@ export default async function PrivacyPolicyPage({params}: PrivacyPolicyPageProps
   return (
     <Container className="flex gap-6 md:gap-8 items-start">
       <Article className="pb-13 lg:pb-23 flex-1">
-        <MiracleBreadcrumbs 
-          locales={routing.locales}
-          overrides={{
-            home: t("breadcrumbs.home"),
-            "privacy-policy": t("breadcrumbs.privacy-policy")
-          }}
-          className="mb-5 md:mb-6"
-        />
-        <Section className="w-full">
-          <MiracleBadge startIcon={<LuCalendar/>} className="mb-4">{t("latestUpdate", {date: formatDate({date: LATEST_UPDATE_DATE, locale})})}</MiracleBadge>
-          <Content />
-        </Section>
+        <MiracleReveal animation="fade-right">
+          <MiracleBreadcrumbs 
+            locales={routing.locales}
+            overrides={{
+              home: t("breadcrumbs.home"),
+              "privacy-policy": t("breadcrumbs.privacy-policy")
+            }}
+            className="mb-5 md:mb-6"
+          />
+          <Section className="w-full">
+            <MiracleBadge startIcon={<LuCalendar/>} className="mb-4">{t("latestUpdate", {date: formatDate({date: LATEST_UPDATE_DATE, locale})})}</MiracleBadge>
+            <Content />
+          </Section>
+        </MiracleReveal> 
       </Article>
       <TableOfContents className="hidden lg:block sticky top-30 shrink-0" />
     </Container>
