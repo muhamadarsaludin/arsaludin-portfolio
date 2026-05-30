9496056747c188892081caf6161d95616ac01cc8
Muhamad Arsaludin <muhamadarsaludin71@gmail.com>
2026-05-30 22:04:56 +0700

    optimize project skeleton

--- features/projects/components/ProjectCardSkeleton.tsx
+++ features/projects/components/ProjectCardSkeleton.tsx
@@ -1,26 +1,39 @@
 import { MiracleSkeleton } from "@/components/miracle/Skeleton"
 import clsx from "clsx"
 
-export default function ProjectCardSkeleton({className}: {className?: string}) {
+export default function ProjectCardSkeleton({ className }: { className?: string }) {
   return (
     <div
       className={clsx(
-        "flex flex-col rounded-2xl border border-primary overflow-hidden",
+        "flex flex-col rounded-2xl border border-primary overflow-hidden bg-card",
         className
       )}
     >
-      <MiracleSkeleton className="aspect-video w-full rounded-none"/>
-      <div className="flex flex-1 flex-col p-6">
-        <MiracleSkeleton className="mb-2.5 h-6 w-3/4"/>
-        <MiracleSkeleton className="mb-2 h-4 w-full" />
-        <MiracleSkeleton className="h-4 w-2/4" />
-        <div className="mt-6 flex flex-wrap items-center gap-2">
-          {[...Array(4)].map((_, j) => (
-            <MiracleSkeleton key={j} className="h-6 w-14" />
+      {/* Skeleton Thumbnail */}
+      <MiracleSkeleton className="aspect-4/3 w-full rounded-none" />
+      
+      {/* Skeleton Body Section */}
+      <div className="flex flex-1 flex-col p-5 md:p-6">
+        {/* Title */}
+        <MiracleSkeleton className="mb-2 h-6 w-3/4 rounded-md" /> 
+
+        {/* Description */}
+        <MiracleSkeleton className="mb-2 h-4 w-full rounded-sm" /> 
+        <MiracleSkeleton className="mb-6 h-4 w-2/4 rounded-sm" /> 
+        
+        {/* Skeleton Skill Badges */}
+        <div className="mt-auto flex flex-wrap items-center gap-2">
+          {Array.from({ length: 4 }).map((_, i) => (
+            <MiracleSkeleton key={`project-skill-skel-${i}`} className="h-6 w-14 rounded-full" />
           ))}
         </div>
       </div>
-      <MiracleSkeleton className="h-10 w-full rounded-none"/>
+      
+      {/* Skeleton Footer Section */}
+      <div className="border-t border-primary bg-secondary px-5 py-3 md:px-6 flex items-center justify-between">
+        <MiracleSkeleton className="h-5 w-5 m-1 rounded-full" variant="med" />
+        <MiracleSkeleton className="h-5 w-5 m-1 rounded-full" variant="med" />
+      </div>
     </div>
   )
 }
